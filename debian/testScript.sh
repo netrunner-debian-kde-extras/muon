611f9e3b78d369bfa2ea92afec54630d7d300b9c
Floris-Andrei Stoica-Marcu <floris.sm@gmail.com>
2014-07-19 03:20:48 +0300

    Made custom testing script

---
+++ debian/testScript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ./obj-x86_64-linux-gnu
+
+output=$(ctest -V -N)
+i=0
+n=0
+
+printf %s "$output" | while IFS= read -r line
+do
+   for word in $line
+    do 
+      if [[ $word == *.shell* ]] 
+	then
+	  $(xvfb-run $word --quiet)
+	  sleep 5
+	  n=$((n + 1))
+	  echo "Running Test $line"
+	  if [[ $? == 0 ]]
+	    then
+	      echo "Passed Test!"
+	      i=$((i + 1))
+	    else
+	      echo "Test failed"
+	      exit $?
+	  fi
+      fi
+   done
+done
